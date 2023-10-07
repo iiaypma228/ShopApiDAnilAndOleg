d7f1c6c0af9efbddb765ea4c2529d433881d56ea
iiaypma228 <loolpip586@gmail.com>
2023-10-08 00:06:14 +0300

    merge

--- Models/AuthOptions.cs
+++ Models/AuthOptions.cs
@@ -7,7 +7,7 @@ public class AuthOptions
 {
     public const string ISSUER = "Retail.Server.API"; // издатель токена
     public const string AUDIENCE = "Retail.Client"; // потребитель токена
-    const string KEY = "Retail.Server.API";   // ключ для шифрации
+    const string KEY = "Retail.Server.API.OlegAndDanilProjectKurs";   // ключ для шифрации
     public const int LIFETIME = 60*24; // время жизни токена в минутах
 
     public static SymmetricSecurityKey GetSymmetricSecurityKey()
